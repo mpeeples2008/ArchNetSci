dea524bc37db01cd29ddc8c50e0814926bac4c0e
Matthew Peeples <mpeeple@asurite.asu.edu>
2022-05-24 15:36:49 -0700

    install.R update

--- install.R
+++ install.R
@@ -1,1 +1,4 @@
-install.packages(c('igraph','statnet','intergraph'))
+packages <- c("igraph", "statnet", "intergraph", "ggraph",
+              "reshape2", "ggmap", "vegan", "sf", "tidyverse")
+
+install.packages(setdiff(packages, rownames(installed.packages())))
