69f4cb36773f2c8ff13185de7b730357787dafdf
Matthew Peeples <Matthew.Peeples@asu.edu>
2022-07-23 15:06:45 -0700

    forgot the 2 in ggplot2

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
-install.packages(c('remotes','renv','ggplot','ggraph','statnet','igraph','intergraph','ggmap','vegan','sf'))
+install.packages(c('remotes','renv','ggplot2','ggraph','statnet','igraph','intergraph','ggmap','vegan','sf'))
 #renv::restore()
