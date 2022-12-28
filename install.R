1a6f33c1693af67ac0d6b34bc77942e6be4048f1
Matthew Peeples <Matthew.Peeples@asu.edu>
2022-12-27 17:24:47 -0700

    update install.R

--- install.R
+++ install.R
@@ -1,6 +1,6 @@
 packages <- c("tidyverse", "ape", "devtools", "igraph", "statnet", "intergraph",
               "tnet", "ggplot2", "rjson", "d3r", "cccd", "networkD3", "visNetwork",
-              "GISTools", "rgeos", "maptools", "sf", "igraphdata", "ggrepel",
+              "rgeos", "maptools", "sf", "igraphdata", "ggrepel",
               "ggsn", "tidyverse", "superheat", "ggplotify", "ggforce", "colorspace",
               "ggmap", "dplyr", "ggpubr", "ggraph", "reshape2", "multinet",
               "RColorBrewer", "Rcpp", "deldir", "vegan", "geosphere", "networkDynamic",
