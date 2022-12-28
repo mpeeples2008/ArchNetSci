2e27981309da917e92a3f9345da83fe8765058d9
Matthew Peeples <Matthew.Peeples@asu.edu>
2022-12-28 09:33:25 -0700

    reduce packages installed on initialization of Binder to minimal required

--- install.R
+++ install.R
@@ -1,14 +1,7 @@
-packages <- c("tidyverse", "ape", "devtools", "igraph", "statnet", "intergraph",
-              "tnet", "ggplot2", "rjson", "d3r", "cccd", "networkD3", "visNetwork",
-              "rgeos", "maptools", "sf", "igraphdata", "ggrepel",
-              "ggsn", "tidyverse", "superheat", "ggplotify", "ggforce", "colorspace",
-              "ggmap", "dplyr", "ggpubr", "ggraph", "reshape2", "multinet",
-              "RColorBrewer", "Rcpp", "deldir", "vegan", "geosphere", "networkDynamic",
-              "scatterplot3d", "patchwork", "concaveman", "latticeExtra",
-              "orca", "pracma", "netdiffuseR", "graphkernels")
+packages <- c("igraph", "statnet", "intergraph", "ggraph",
+              "reshape2", "ggmap", "vegan", "sf")
 
 install.packages(setdiff(packages, rownames(installed.packages())))
-
 devtools::install_github("liamgilbey/ggwaffle")
 devtools::install_github("QiliShi/NetworkSim")
 
