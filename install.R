d36cc2e80ba00aff56a925f923008dde21a3aba1
Matthew Peeples <Matthew.Peeples@asu.edu>
2022-12-28 15:17:43 -0700

    docker test

--- install.R
+++ install.R
@@ -1,7 +1,2 @@
-packages <- c("igraph", "statnet", "intergraph", "ggraph",
-              "reshape2", "ggmap", "vegan", "sf", "devtools")
 
-install.packages(setdiff(packages, rownames(installed.packages())))
-devtools::install_github("liamgilbey/ggwaffle")
-devtools::install_github("QiliShi/NetworkSim")
 
