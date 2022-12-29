132c4535dfe5969725c67306192d1339b39c3841
Matthew Peeples <Matthew.Peeples@asu.edu>
2022-12-28 19:27:25 -0700

    renv restore on update binder

--- install.R
+++ install.R
@@ -1,4 +1,1 @@
-packages <- c("igraph", "statnet", "intergraph", "ggraph",
-              "reshape2", "ggmap", "vegan", "sf")
-
-install.packages(setdiff(packages, rownames(installed.packages())))
+renv::restore()
