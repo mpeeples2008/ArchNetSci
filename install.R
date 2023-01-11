1a0d324cc7d687ab87b9ecf07589d325bc345d8d
Matthew Peeples <Matthew.Peeples@asu.edu>
2023-01-10 22:16:23 -0700

    update package install

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 packages <- c("igraph", "statnet", "intergraph", "ggraph",
               "reshape2", "ggmap", "vegan", "sf", "multinet",
-              "ggpubr", "Matrix", "concaveman", "ggplotify")
+              "ggpubr", "concaveman", "ggplotify")
 
 install.packages(setdiff(packages, rownames(installed.packages())))
